40cc5f6f9748e2dfe4e39ed641e30279bd383db5
Marco Rosetti <marco.rosetti@teachat.it>
2019-09-05 10:16:36 +0200

    Fix clean old scripts

--- clean-old-backups.sh
+++ clean-old-backups.sh
@@ -9,57 +9,60 @@ putdir=$FTP_BACKUP_FOLDER
 ndays=$BACKUP_DAYS
 
 # work out our cutoff date
-MM=`date --date="$ndays days ago" +%b`
-DD=`date --date="$ndays days ago" +%d`
+MM=$(date --date="$ndays days ago" +%b)
+DD=$(date --date="$ndays days ago" +%d)
 
 echo removing files older than $MM $DD
 MONTH_NUMBER=$(date -d "$MM $DD" '+%m')
-MONTH_NUMBER=$(tr -dc '0-9' <<< $MONTH_NUMBER)
-DD=$(tr -dc '0-9' <<< $DD)
+MONTH_NUMBER=$(tr -dc '0-9' <<<$MONTH_NUMBER)
+DD=$(tr -dc '0-9' <<<$DD)
 
 MONTH_NUMBER=${MONTH_NUMBER#0}
 DD=${DD#0}
 
-listing=`lftp -u $ftpuser,$ftppass $ftpsite << EOF
+listing=$(
+  lftp -u $ftpuser,$ftppass $ftpsite <<EOF
 cd $putdir
 ls
 bye
 EOF
-`
+)
 
-lista=( $listing )
+lista=($listing)
 
 # loop over our files
-for ((FNO=0; FNO<${#lista[@]}; FNO+=9));do
+for ((FNO = 0; FNO < ${#lista[@]}; FNO += 9)); do
   # month (element 5), day (element 6) and filename (element 8)
-  #echo Date ${lista[`expr $FNO+5`]} ${lista[`expr $FNO+6`]} File: ${lista[`expr $FNO+8`]}
-   # echo FNO $FNO
+  # echo Date ${lista[$(expr $FNO+5)]} ${lista[$(expr $FNO+6)]} File: ${lista[$(expr $FNO+8)]}
+  # echo FNO $FNO
   # check the date stamp
-   FILE_MONTH=$(date -d "${lista[`expr $FNO+5`]} ${lista[`expr $FNO+6`]}" '+%m')
-FILE_MONTH=$(tr -dc '0-9' <<< $FILE_MONTH)
-FILE_MONTH=${FILE_MONTH#0}
-    if ((FILE_MONTH < MONTH_NUMBER));
-    then 
- echo "Removing ${lista[`expr $FNO+8`]}"
-      lftp -u $ftpuser,$ftppass $ftpsite <<EOMYF
+  FILE_MONTH=$(date -d "${lista[$(expr $FNO+5)]} ${lista[$(expr $FNO+6)]}" '+%m')
+  FILE_MONTH=$(tr -dc '0-9' <<<$FILE_MONTH)
+  FILE_MONTH=${FILE_MONTH#0}
+  if ((FILE_MONTH < MONTH_NUMBER)); then
+    echo "Removing ${lista[$(expr $FNO+8)]} cause previous month"
+    lftp -u $ftpuser,$ftppass $ftpsite <<EOMYF
       cd $putdir
-      rm ${lista[`expr $FNO+8`]}
+      rm ${lista[$(expr $FNO+8)]}
       bye
 EOMYF
-    fi
+  fi
 
-   if [ ${lista[`expr $FNO+5`]}=$MM ];
-   then
-     if [[ ${lista[`expr $FNO+6`]#0} -lt $DD ]];
-     then
+  # echo Month ${lista[$(expr $FNO+5)]}
+  # echo MM $MM
+  #if [ ${lista[`expr $FNO+5`]}=$MM ];
+  if ((FILE_MONTH == MONTH_NUMBER)); then
+    # echo Day ${lista[$(expr $FNO+6)]#0}
+    # echo DD: $DD
+    if [[ ${lista[$(expr $FNO+6)]#0} -lt $DD ]]; then
       # Remove this file
-      echo "Removing ${lista[`expr $FNO+8`]}"
+      echo "Removing ${lista[$(expr $FNO+8)]}"
       lftp -u $ftpuser,$ftppass $ftpsite <<EOMYF
       cd $putdir
-      rm ${lista[`expr $FNO+8`]}
+      rm ${lista[$(expr $FNO+8)]}
       bye
 EOMYF
 
-     fi
-   fi
+    fi
+  fi
 done
